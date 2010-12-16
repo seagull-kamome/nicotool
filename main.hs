4eb0b438740070df7e1e227e4e23437fcc0183ad
Hiroki Hattori <seagull.kamome@gmail.com>
2010-12-16 22:42:06 +0900

    放送の情報を表示するようにした

--- main.hs
+++ main.hs
@@ -1,7 +1,8 @@
+#! /usr/bin/runhaskell -threaded
 {-# LANGUAGE RecordWildCards #-}
 
 import Control.Concurrent.MVar (newMVar, modifyMVar_)
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, yield)
 import Control.Exception (bracket)
 import qualified System.Glib.Types
 import qualified Graphics.UI.Gtk as G
@@ -9,6 +10,7 @@ import System.Time (diffClockTimes, normalizeTimeDiff, TimeDiff (..))
 import System.IO (openFile, hClose, IOMode (ReadMode) )
 import System.IO.UTF8 (hGetContents)
 import System.Directory (getAppUserDataDirectory)
+import Text.Printf (printf)
 
 import qualified Network.Service.NicoVideo.Alart as AL
 import qualified Network.Service.NicoVideo.Chat as CH
@@ -20,8 +22,10 @@ main :: IO ()
 main = do pref <- loadPreference
           --
           --
-          --                  
+          --
           G.initGUI
+          G.timeoutAddFull (yield >> return True) G.priorityDefaultIdle 100 -- これが無いとフォーカスが無い時に裏スレッドが止まる
+
           builder <- G.builderNew
           G.builderAddFromFile builder "nicotool.GtkBuilder"
           let getW :: (System.Glib.Types.GObjectClass b) => (G.GObject -> b) -> String -> IO b
@@ -31,12 +35,12 @@ main = do pref <- loadPreference
           G.onDestroy mainwindow G.mainQuit
           bindAction "quitApplication" $ G.widgetDestroy mainwindow
           bindAction "showAbout" $ G.builderGetObject builder G.castToAboutDialog "AboutDialog" >>= G.dialogRun >> return ()
-          
+
           let setupClm store name f = do col <- getW G.castToTreeViewColumn name
                                          ren <- G.cellRendererTextNew
                                          G.treeViewColumnPackStart col ren True
                                          G.cellLayoutSetAttributes col ren store $ \x -> [ G.cellText G.:= f x ]
-          
+
           --
           --
           --
@@ -44,14 +48,16 @@ main = do pref <- loadPreference
           chatStore <- G.listStoreNew []
           G.treeViewSetModel chatView chatStore
           setupClm chatStore "ChatNo" $ show . CH.chatResNo . snd
-          setupClm chatStore "ChatVPos" $ \(x,y) -> let TimeDiff {..} = normalizeTimeDiff $ diffClockTimes (CH.chatDate y)  x in (show tdMin) ++ ":" ++ (show tdSec)
+          setupClm chatStore "ChatVPos" $ \(x,y) -> let TimeDiff {..} = normalizeTimeDiff $ diffClockTimes (CH.chatDate y)  x in printf "%02d:%02d" tdMin tdSec
           setupClm chatStore "ChatUserID" $ CH.chatUserID . snd
           setupClm chatStore "ChatMessage" $ CH.chatMessage . snd
           setupClm chatStore "ChatPremium" $ show . CH.chatIsPremium . snd
           setupClm chatStore "ChatMail" $ CH.chatMail . snd
-          
-          
+
+
           currStreamVar <- newMVar Nothing
+          imgCommunity <- getW G.castToImage "imgCommunity"
+          lblStreamInfo <- getW G.castToLabel "lblStreamInfo"
           let closeStream = modifyMVar_ currStreamVar $ \x -> maybe (return ()) CH.stopChat x >> return Nothing
           bindAction "closeStream" closeStream
           bindAction "openStream" $ do
@@ -62,7 +68,7 @@ main = do pref <- loadPreference
             txtStreamID <- getW G.castToEntry "txtStreamID"
             res <- G.dialogRun dlg
             G.widgetHide dlg
-            if res /= G.ResponseOk 
+            if res /= G.ResponseOk
               then return ()
               else do streamID <- G.entryGetText txtStreamID
                       ps' <- NV.getPlayerStatus streamID cookieLoader -- 動画の情報を得る
@@ -72,10 +78,13 @@ main = do pref <- loadPreference
                           let basetime = NV.liveStartTime $ playerStatusLiveStream
                           in modifyMVar_ currStreamVar $ \x -> do maybe (return ()) CH.stopChat x
                                                                   G.listStoreClear chatStore
-                                                                  client <- CH.watchChat playerStatusMessageServer 
-                                                                            defaultChatLogNum 
+                                                                  client <- CH.watchChat playerStatusMessageServer
+                                                                            defaultChatLogNum
                                                                             (\_ msg -> appendLog chatView chatStore (basetime, msg) )
                                                                             (const (return ()))
+                                                                  --
+                                                                  G.set lblStreamInfo [ G.labelLabel G.:= ("<span font_size='x-large'>" ++ NV.liveTitle playerStatusLiveStream) ++ "</span>\n\n" ++ (NV.liveDescription playerStatusLiveStream)]
+                                                                  --
                                                                   return $ Just client
 
 
@@ -93,11 +102,10 @@ main = do pref <- loadPreference
                                      (\(liveid, comid, ownerid) -> forkIO (do si' <- NV.getStreamInfo $ "lv" ++ liveid
                                                                               case si' of
                                                                                 Left err -> print err
-                                                                                Right (NV.StreamInfo {..}, comname, thumbnail, ads) -> do
+                                                                                Right (NV.StreamInfo {..}, comname, thumbnail, ads) ->
                                                                                   appendLog alartView alartStore (streamLiveID, streamTitle, comname, ownerid)
-                                                                                  return ()
                                                                           ) >> return () )
-            (return ())
+                                     (return ())
 
           G.widgetShowAll mainwindow
           G.mainGUI
@@ -123,4 +131,4 @@ loadPreference :: IO Preference
 loadPreference = do dir <- getAppUserDataDirectory "nicotool"
                     h <- openFile (dir ++ "/preference") ReadMode
                     hGetContents h >>= return . read
-                    
+
